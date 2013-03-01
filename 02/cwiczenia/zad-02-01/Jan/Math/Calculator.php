086bd64f2a674fb2944ef8d119f53cc33acb8575
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-03-01 12:51:35 +0100

    zad-02-01

---
+++ 02/cwiczenia/zad-02-01/Jan/Math/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Jan\Math\Calculator;
+
+class Calculator
+{
+    public static function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ 02/cwiczenia/zad-02-01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta charset="UTF-8" />
+  </head>
+<body>
+
+<?php
+
+require_once 'Jan/Math/Calculator.php';
+
+use Jan\Math\Calculator;
+
+echo '3 + 11 = ';
+echo Calculator::add(3, 11);
+
+
+?>
+
+</body>
+</html>
