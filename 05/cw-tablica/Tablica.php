2ef47f1cd3c3239a70e263b2aa1c7e8136357c6f
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-03-22 14:03:40 +0100

    po cwiczeniach 1

---
+++ 05/cw-tablica/Tablica.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Gajdaw\Util;
+
+class Tablica
+{
+    public function setItems($items)
+    {
+        throw new \RuntimeException('Not implemented yet');
+    }
+}
---
+++ 05/cw-tablica/Tests/TablicaTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Gajdaw\Util\Tests;
+class TablicaTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSetItems()
+    {
+
+        $this->markTestIncomplete('Not implemented');
+    }
+}
