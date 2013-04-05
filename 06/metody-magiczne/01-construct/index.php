0d3f03eb4e2a48765f703b62177a566296034d5c
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-04-05 15:14:54 +0200

    wip

---
+++ 06/metody-magiczne/01-construct/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Metody magiczne</title>
+    <meta charset="UTF-8" />
+  </head>
+<body>
+
+<?php
+
+require_once 'Osoba.php';
+
+$o= new Osoba('Jan');
+
+echo $o->getImie();
+
+
+
+?>
+
+
+</body>
+</html>
